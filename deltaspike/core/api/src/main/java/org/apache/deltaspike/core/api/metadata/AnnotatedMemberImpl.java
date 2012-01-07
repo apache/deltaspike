4a4fad76a9cdce04bb63b7438655c71017466cdb
Jason Porter <lightguardjp@apache.org>
2012-01-06 23:12:10 -0700

    [DELTASPIKE-45] Adding AnnotatedTypeBuilder and needed classes
    I think we need to go over these and make sure everything is good.

---
+++ deltaspike/core/api/src/main/java/org/apache/deltaspike/core/api/metadata/AnnotatedMemberImpl.java
@@ -0,0 +1,58 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements. See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership. The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License. You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied. See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+package org.apache.deltaspike.core.api.metadata;
+
+import javax.enterprise.inject.spi.AnnotatedMember;
+import javax.enterprise.inject.spi.AnnotatedType;
+import java.lang.reflect.Member;
+import java.lang.reflect.Modifier;
+import java.lang.reflect.Type;
+
+/**
+ *
+ */
+abstract class AnnotatedMemberImpl<X, M extends Member> extends AnnotatedImpl implements AnnotatedMember<X>
+{
+    private final AnnotatedType<X> declaringType;
+    private final M javaMember;
+
+    protected AnnotatedMemberImpl(AnnotatedType<X> declaringType, M member, Class<?> memberType,
+                                  AnnotationStore annotations, Type genericType, Type overridenType)
+    {
+        super(memberType, annotations, genericType, overridenType);
+        this.declaringType = declaringType;
+        this.javaMember = member;
+    }
+
+    public AnnotatedType<X> getDeclaringType()
+    {
+        return declaringType;
+    }
+
+    public M getJavaMember()
+    {
+        return javaMember;
+    }
+
+    public boolean isStatic()
+    {
+        return Modifier.isStatic(javaMember.getModifiers());
+    }
+
+}
