c7a5550121b0de0597fb41f7c6b8311b3c7aac7b
Jason Porter <lightguardjp@apache.org>
2012-01-12 23:53:52 -0700

    [DELTASPIKE-46] Adding tests for AnnotatedTypeBuilder
    Also adding some AnnotationLiterals for use in the test and docs.

---
+++ deltaspike/core/api/src/test/java/org/apache/deltaspike/test/api/metadata/Cat.java
@@ -0,0 +1,67 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements. See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership. The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License. You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied. See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
+package org.apache.deltaspike.test.api.metadata;
+
+import javax.annotation.PostConstruct;
+import javax.inject.Inject;
+import javax.inject.Named;
+
+@Named("cat")
+public class Cat
+{
+    private String color;
+    private String gender;
+
+    public Cat()
+    {
+    }
+
+    @Inject
+    public Cat(String color, String gender)
+    {
+        this.color = color;
+        this.gender = gender;
+    }
+
+    @PostConstruct
+    public void setup()
+    {
+    }
+
+    public String getColor()
+    {
+        return color;
+    }
+
+    public void setColor(String color)
+    {
+        this.color = color;
+    }
+
+    public String getGender()
+    {
+        return gender;
+    }
+
+    public void setGender(String gender)
+    {
+        this.gender = gender;
+    }
+}
