4fe5dbc0339bd3982b3ef107cffe5611c9d110a9
gpetracek <gpetracek@apache.org>
2012-02-21 14:49:05 +0100

    DELTASPIKE-92 and DELTASPIKE-102

--- deltaspike/cdise/api/src/main/java/org/apache/deltaspike/cdise/api/CdiContainerLoader.java
+++ deltaspike/cdise/api/src/main/java/org/apache/deltaspike/cdise/api/CdiContainerLoader.java
@@ -47,14 +47,34 @@ public final class CdiContainerLoader
         }
         else 
         {
-            throw new RuntimeException("Could not find a CdiContainer available in the classpath!");
+            throw new IllegalStateException("Could not find an implementation of " + CdiContainer.class.getName() +
+                " available in the classpath!");
         }
         
         if (cdiIt.hasNext())
         {
-            throw new RuntimeException("Too many CdiContainer found in the classpath!");
+            String foundContainers = getContainerDetails();
+            throw new IllegalStateException("Too many implementations of " + CdiContainer.class.getName() +
+                " found in the classpath! Details: " + foundContainers);
         }
         
         return testContainer;
+    }
+
+    private static String getContainerDetails()
+    {
+        StringBuilder result = new StringBuilder();
+
+        Class containerClass;
+        for (CdiContainer cdiContainer : ServiceLoader.load(CdiContainer.class))
+        {
+            containerClass = cdiContainer.getClass();
+            result.append(containerClass.getProtectionDomain().getCodeSource().getLocation().toExternalForm());
+            result.append(containerClass.getName());
+
+            result.append(System.getProperty("line.separator"));
+        }
+
+        return result.toString();
     }
 }
