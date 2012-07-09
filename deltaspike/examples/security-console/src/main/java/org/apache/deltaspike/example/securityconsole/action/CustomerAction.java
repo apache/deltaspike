e44156c2f39966f13c733baffb5f5bf2b095dc45
Shane Bryzak <sbryzak@gmail.com>
2012-07-09 10:01:41 +1000

    example application changes

---
+++ deltaspike/examples/security-console/src/main/java/org/apache/deltaspike/example/securityconsole/action/CustomerAction.java
@@ -0,0 +1,9 @@
+package org.apache.deltaspike.example.securityconsole.action;
+
+import javax.enterprise.inject.Model;
+
+@Model 
+public class CustomerAction 
+{
+
+}
--- deltaspike/examples/security-console/src/main/java/org/apache/deltaspike/example/securityconsole/model/Customer.java
+++ deltaspike/examples/security-console/src/main/java/org/apache/deltaspike/example/securityconsole/model/Customer.java
@@ -46,4 +46,9 @@ public class Customer implements Serializable
     {
         this.lastName = lastName;
     }
+    
+    public String toString()
+    {
+        return String.format("%d: %s %s", id, firstName, lastName);
+    }
 }
