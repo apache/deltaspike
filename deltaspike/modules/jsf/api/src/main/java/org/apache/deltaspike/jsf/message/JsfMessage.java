737771ba18bca7bdd3e8684f06f127b6f16f6e18
Mark Struberg <struberg@apache.org>
2012-10-06 23:45:40 +0200

    DELTASPIKE-277 add JsfMessage API

---
+++ deltaspike/modules/jsf/api/src/main/java/org/apache/deltaspike/jsf/message/JsfMessage.java
@@ -0,0 +1,48 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements. See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership. The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License. You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied. See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+package org.apache.deltaspike.jsf.message;
+
+/**
+ * <p>An injectable component for typesafe FacesMessages.
+ * T must be a class which is annotated with
+ * {@link org.apache.deltaspike.core.api.message.annotation.MessageBundle}</p>
+ *
+ * <p>Usage:
+ * <pre>
+ * &#064;Inject
+ * JsfMessage&lt;MyMessages&gt; msg;
+ * ...
+ * msg.addError().userNotLoggedIn();
+ * </pre>
+ * <p>MessageBundle methods which are used as JsfMessage can return a
+ * {@link org.apache.deltaspike.core.api.message.Message} or a String.
+ * In case of a String we use it for both the summary and detail
+ * information on the FacesMessage. </p>
+ * <p>If a Message is returned, we lookup the 'detail' and 'summary'
+ * categories (see {@link org.apache.deltaspike.core.api.message.Message#toString(String)}
+ * for creating the FacesMessage.</p>
+ *
+ */
+public interface JsfMessage<T>
+{
+    T addError();
+    T addFatal();
+    T addInfo();
+    T addWarn();
+}
