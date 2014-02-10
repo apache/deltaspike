fd5beb5b201acde0664f8fbde0f87c318e0df99d
Thomas Hug <Thomas.Hug@ctp.com>
2014-02-10 12:54:40 +0100

    DELTASPIKE-518 Refactored test for Arquillian

--- deltaspike/modules/data/impl/src/test/java/org/apache/deltaspike/data/impl/util/jpa/QueryStringExtractorFactoryTest.java
+++ deltaspike/modules/data/impl/src/test/java/org/apache/deltaspike/data/impl/util/jpa/QueryStringExtractorFactoryTest.java
@@ -18,48 +18,100 @@
  */
 package org.apache.deltaspike.data.impl.util.jpa;
 
-import org.junit.Assert;
-import org.junit.Test;
+import static java.lang.Thread.currentThread;
+import static java.lang.reflect.Proxy.newProxyInstance;
+import static org.apache.deltaspike.data.test.util.TestDeployments.initDeployment;
+import static org.junit.Assert.assertEquals;
 
-import javax.persistence.PersistenceException;
-import javax.persistence.Query;
 import java.lang.reflect.InvocationHandler;
 import java.lang.reflect.Method;
 import java.lang.reflect.Proxy;
 
+import javax.inject.Inject;
+import javax.persistence.PersistenceException;
+import javax.persistence.Query;
+
+import org.apache.deltaspike.test.category.WebProfileCategory;
+import org.jboss.arquillian.container.test.api.Deployment;
+import org.jboss.arquillian.junit.Arquillian;
+import org.jboss.shrinkwrap.api.Archive;
+import org.junit.Test;
+import org.junit.experimental.categories.Category;
+import org.junit.runner.RunWith;
+
+@RunWith(Arquillian.class)
+@Category(WebProfileCategory.class)
 public class QueryStringExtractorFactoryTest
 {
+
+    private static final String QUERY_STRING = "it works";
+
+    @Inject
+    private QueryStringExtractorFactory factory;
+
+    @Deployment
+    public static Archive<?> deployment()
+    {
+        return initDeployment();
+    }
 
     @Test
     public void should_unwrap_query_even_proxied()
     {
-        // given
-
         // when
-        String extractor = new QueryStringExtractorFactory().extract((Query) Proxy.newProxyInstance(Thread.currentThread().getContextClassLoader(), new Class[]{Query.class}, new InvocationHandler() {
-            @Override
-            public Object invoke(Object proxy, Method method, Object[] args) throws Throwable {
-                if (method.getName().equals("toString")) return "Unknown provider wrapper for tests.";
-                if (method.getName().equals("unwrap")) {
-                    Class<?> clazz = (Class<?>) args[0];
-                    if (clazz.getName().contains("hibernate") || clazz.getName().contains("openjpa") || clazz.getName().contains("eclipse")) {
-                        return Proxy.newProxyInstance(Thread.currentThread().getContextClassLoader(), new Class[]{clazz}, new InvocationHandler() {
-                            @Override
-                            public Object invoke(Object proxy, Method method, Object[] args) throws Throwable {
-                                if (method.getName().equals("getQueryString")) return "it works";
-                                return null;
-                            }
-                        }); // we don't care of teh result actually
-                    } else {
-                        throw new PersistenceException("Unable to unwrap for " + clazz);
+        String extracted = factory.extract((Query) newProxyInstance(currentThread().getContextClassLoader(),
+                        new Class[] { Query.class }, new InvocationHandler()
+            {
+                @Override
+                public Object invoke(Object proxy, Method method, Object[] args) throws Throwable
+                {
+                    if (method.getName().equals("toString"))
+                    {
+                        return "Unknown provider wrapper for tests.";
                     }
+                    if (method.getName().equals("unwrap"))
+                    {
+                        Class<?> clazz = (Class<?>) args[0];
+                        if (clazz.getName().contains("hibernate") ||
+                                clazz.getName().contains("openjpa") ||
+                                clazz.getName().contains("eclipse"))
+                        {
+                            return createProxy(clazz);
+                        }
+                        else
+                        {
+                            throw new PersistenceException("Unable to unwrap for " + clazz);
+                        }
+                    }
+                    return null;
                 }
-                return null;
-            }
-        }));
+            })
+        );
 
         // then
-        Assert.assertEquals("it works", extractor);
+        assertEquals(QUERY_STRING, extracted);
+    }
+
+    private static Object createProxy(Class<?> forClass)
+    {
+        return Proxy.newProxyInstance(currentThread().getContextClassLoader(),
+                new Class[] { forClass },
+                new FakeQueryInvocationHandler());
+    }
+
+    private static class FakeQueryInvocationHandler implements InvocationHandler
+    {
+
+        @Override
+        public Object invoke(Object proxy, Method method, Object[] args) throws Throwable
+        {
+            if (!method.getReturnType().equals(String.class))
+            {
+                return createProxy(method.getReturnType());
+            }
+            return QUERY_STRING; // we don't care of the result actually
+        }
+
     }
 
 }
