9cb3fe2e4e9acf944fe6d85de203047eb5970273
John D. Ament <johndament@apache.org>
2014-08-05 22:42:26 -0400

    DELTASPIKE-653 Copyright headers.

--- deltaspike/cdictrl/servlet/src/test/java/org/apache/deltaspike/cdise/servlet/test/EmbeddedServletContainer.java
+++ deltaspike/cdictrl/servlet/src/test/java/org/apache/deltaspike/cdise/servlet/test/EmbeddedServletContainer.java
@@ -1,19 +1,25 @@
-/*******************************************************************************
- * Copyright (c) 2013 - 2014 Sparta Systems, Inc.
- ******************************************************************************/
-
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements. See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership. The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License. You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied. See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
 package org.apache.deltaspike.cdise.servlet.test;
 
-import io.undertow.Undertow;
-import io.undertow.servlet.Servlets;
-import io.undertow.servlet.api.DeploymentInfo;
-import io.undertow.servlet.api.DeploymentManager;
-import io.undertow.servlet.api.ListenerInfo;
-import io.undertow.servlet.api.ServletInfo;
 import org.apache.deltaspike.cdise.api.CdiContainer;
 import org.apache.deltaspike.cdise.api.CdiContainerLoader;
-import org.apache.deltaspike.cdise.servlet.CdiServletRequestListener;
-import org.apache.deltaspike.cdise.servlet.test.content.RequestServlet;
 import org.apache.http.HttpResponse;
 import org.apache.http.client.methods.HttpGet;
 import org.apache.http.impl.client.DefaultHttpClient;
