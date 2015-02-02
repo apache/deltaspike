934a2d9cb6a850b3065828a1c4788f8e8725ebe7
Jozef Hartinger <jharting@redhat.com>
2015-02-02 15:56:33 +0100

    DELTASPIKE-818 CDI Events with reception condition in WindowScoped beans

--- deltaspike/core/api/src/main/java/org/apache/deltaspike/core/util/context/AbstractContext.java
+++ deltaspike/core/api/src/main/java/org/apache/deltaspike/core/util/context/AbstractContext.java
@@ -96,7 +96,7 @@ public abstract class AbstractContext implements Context
     {
         if (creationalContext == null)
         {
-            return null;
+            return get(bean);
         }
         
         checkActive();
