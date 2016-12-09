43bdf6ae593aa568e9a40a66215bf0a70659946f
Mark Struberg <struberg@apache.org>
2016-12-09 10:31:24 +0100

    DELTASPIKE-1227 also add Servlet-3.0 methods to mock
    Helps if you have a module with both cdictrl tests + real tomcat integration tests

--- deltaspike/cdictrl/impl-owb/src/main/java/org/apache/deltaspike/cdise/owb/MockServletContext.java
+++ deltaspike/cdictrl/impl-owb/src/main/java/org/apache/deltaspike/cdise/owb/MockServletContext.java
@@ -22,16 +22,24 @@ import java.io.InputStream;
 import java.net.MalformedURLException;
 import java.net.URL;
 import java.util.Enumeration;
+import java.util.EventListener;
 import java.util.Hashtable;
+import java.util.Map;
 import java.util.Set;
 import java.util.StringTokenizer;
 
 import javax.enterprise.inject.Typed;
+import javax.servlet.Filter;
+import javax.servlet.FilterRegistration;
 import javax.servlet.RequestDispatcher;
 import javax.servlet.Servlet;
 import javax.servlet.ServletContext;
 import javax.servlet.ServletContextEvent;
 import javax.servlet.ServletException;
+import javax.servlet.ServletRegistration;
+import javax.servlet.SessionCookieConfig;
+import javax.servlet.SessionTrackingMode;
+import javax.servlet.descriptor.JspConfigDescriptor;
 
 /**
  * Mock ServletContext needed to startup the container.
@@ -60,6 +68,9 @@ public class MockServletContext implements ServletContext
     {
         return new ServletContextEvent(getInstance());
     }
+
+
+
 
 
 
@@ -187,5 +198,167 @@ public class MockServletContext implements ServletContext
     public void setAttribute(String name, Object object)
     {
         attributes.put(name, object);
+    }
+
+    @Override
+    public boolean setInitParameter(String name, String value)
+    {
+        return false;
+    }
+
+    @Override
+    public ServletRegistration.Dynamic addServlet(String servletName, String className)
+        throws IllegalArgumentException, IllegalStateException
+    {
+        return null;
+    }
+
+    @Override
+    public ServletRegistration.Dynamic addServlet(String servletName, Servlet servlet)
+        throws IllegalArgumentException, IllegalStateException
+    {
+        return null;
+    }
+
+    @Override
+    public ServletRegistration.Dynamic addServlet(String servletName, Class<? extends Servlet> clazz)
+        throws IllegalArgumentException, IllegalStateException
+    {
+        return null;
+    }
+
+    @Override
+    public <T extends Servlet> T createServlet(Class<T> clazz) throws ServletException
+    {
+        return null;
+    }
+
+    @Override
+    public ServletRegistration getServletRegistration(String servletName)
+    {
+        return null;
+    }
+
+    @Override
+    public Map<String, ? extends ServletRegistration> getServletRegistrations()
+    {
+        return null;
+    }
+
+    @Override
+    public FilterRegistration.Dynamic addFilter(String filterName, String className)
+        throws IllegalArgumentException, IllegalStateException
+    {
+        return null;
+    }
+
+    @Override
+    public FilterRegistration.Dynamic addFilter(String filterName, Filter filter)
+        throws IllegalArgumentException, IllegalStateException
+    {
+        return null;
+    }
+
+    @Override
+    public FilterRegistration.Dynamic addFilter(String filterName, Class<? extends Filter> filterClass)
+        throws IllegalArgumentException, IllegalStateException
+    {
+        return null;
+    }
+
+    @Override
+    public <T extends Filter> T createFilter(Class<T> clazz) throws ServletException
+    {
+        return null;
+    }
+
+    @Override
+    public FilterRegistration getFilterRegistration(String filterName)
+    {
+        return null;
+    }
+
+    @Override
+    public Map<String, ? extends FilterRegistration> getFilterRegistrations()
+    {
+        return null;
+    }
+
+    @Override
+    public void addListener(Class<? extends EventListener> listenerClass)
+    {
+
+    }
+
+    @Override
+    public void addListener(String className)
+    {
+
+    }
+
+    @Override
+    public <T extends EventListener> void addListener(T t)
+    {
+
+    }
+
+    @Override
+    public <T extends EventListener> T createListener(Class<T> clazz) throws ServletException
+    {
+        return null;
+    }
+
+    @Override
+    public void declareRoles(String... roleNames)
+    {
+
+    }
+
+    @Override
+    public SessionCookieConfig getSessionCookieConfig()
+    {
+        return null;
+    }
+
+    @Override
+    public void setSessionTrackingModes(Set<SessionTrackingMode> sessionTrackingModes)
+    {
+
+    }
+
+    @Override
+    public Set<SessionTrackingMode> getDefaultSessionTrackingModes()
+    {
+        return null;
+    }
+
+    @Override
+    public int getEffectiveMajorVersion() throws UnsupportedOperationException
+    {
+        return 0;
+    }
+
+    @Override
+    public int getEffectiveMinorVersion() throws UnsupportedOperationException
+    {
+        return 0;
+    }
+
+    @Override
+    public Set<SessionTrackingMode> getEffectiveSessionTrackingModes()
+    {
+        return null;
+    }
+
+    @Override
+    public ClassLoader getClassLoader()
+    {
+        return null;
+    }
+
+    @Override
+    public JspConfigDescriptor getJspConfigDescriptor()
+    {
+        return null;
     }
 }
