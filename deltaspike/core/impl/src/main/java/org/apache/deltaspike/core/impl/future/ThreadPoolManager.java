c4006cc3b5fda1eea5551fee5a0a7e15c19264d9
Romain Manni-Bucau <rmannibucau@apache.org>
2017-11-26 15:31:46 +0100

    DELTASPIKE-1302 configuration for ThreadPoolManager

--- deltaspike/core/impl/src/main/java/org/apache/deltaspike/core/impl/future/ThreadPoolManager.java
+++ deltaspike/core/impl/src/main/java/org/apache/deltaspike/core/impl/future/ThreadPoolManager.java
@@ -18,22 +18,47 @@
  */
 package org.apache.deltaspike.core.impl.future;
 
+import org.apache.deltaspike.core.api.config.ConfigResolver;
 import org.apache.deltaspike.core.api.config.base.CoreBaseConfig;
 
 import javax.annotation.PreDestroy;
 import javax.enterprise.context.ApplicationScoped;
+import javax.enterprise.context.spi.CreationalContext;
+import javax.enterprise.inject.spi.Bean;
+import javax.enterprise.inject.spi.BeanManager;
+import javax.inject.Inject;
+import javax.naming.InitialContext;
+import javax.naming.NamingException;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Set;
+import java.util.concurrent.ArrayBlockingQueue;
+import java.util.concurrent.BlockingQueue;
 import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.ConcurrentMap;
+import java.util.concurrent.Executor;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
+import java.util.concurrent.LinkedBlockingDeque;
+import java.util.concurrent.LinkedBlockingQueue;
+import java.util.concurrent.RejectedExecutionHandler;
+import java.util.concurrent.SynchronousQueue;
+import java.util.concurrent.ThreadFactory;
+import java.util.concurrent.ThreadPoolExecutor;
 import java.util.concurrent.TimeUnit;
+
+import static java.util.Arrays.asList;
+import static java.util.Locale.ENGLISH;
 
 @ApplicationScoped
 public class ThreadPoolManager
 {
     private final ConcurrentMap<String, ExecutorService> pools = new ConcurrentHashMap<String, ExecutorService>();
-    private volatile ExecutorService defaultPool;
+    private final Collection<CreationalContext<?>> contexts = new ArrayList<CreationalContext<?>>(8);
     private volatile boolean closed = false;
+
+    @Inject
+    private BeanManager beanManager;
 
     @PreDestroy
     private void shutdown()
@@ -43,6 +68,9 @@ public class ThreadPoolManager
         for (final ExecutorService es : pools.values())
         {
             es.shutdown();
+        }
+        for (final ExecutorService es : pools.values())
+        {
             try
             {
                 es.awaitTermination(timeout, TimeUnit.MILLISECONDS);
@@ -52,19 +80,13 @@ public class ThreadPoolManager
                 Thread.interrupted();
             }
         }
-        if (defaultPool != null)
-        {
-            defaultPool.shutdown();
-            try
-            {
-                defaultPool.awaitTermination(timeout, TimeUnit.MILLISECONDS);
-            }
-            catch (final InterruptedException e)
-            {
-                Thread.interrupted();
-            }
-        }
         pools.clear();
+
+        for (final CreationalContext<?> ctx : contexts)
+        {
+            ctx.release();
+        }
+        contexts.clear();
     }
 
     public ExecutorService find(final String name)
@@ -76,25 +98,148 @@ public class ThreadPoolManager
         ExecutorService pool = pools.get(name);
         if (pool == null)
         {
-            ensureDefaultPool();
-            pool = defaultPool;
+            synchronized (this)
+            {
+                pool = pools.get(name);
+                if (pool == null)
+                {
+                    // the instantiation does the following:
+                    // 1. check if there is a named bean matching this name using @Default qualifier
+                    // 2. check if there is a JNDI entry (ManagedExecutorService case) matching this name
+                    // 3. create a new executor service based on the DS-config
+
+                    // 1.
+                    final Set<Bean<?>> beans = beanManager.getBeans(name);
+                    if (beans != null && !beans.isEmpty())
+                    {
+                        final Bean<?> bean = beanManager.resolve(beans);
+                        if (bean.getTypes().contains(ExecutorService.class))
+                        {
+                            final CreationalContext<Object> creationalContext = beanManager.createCreationalContext(null);
+                            if (!beanManager.isNormalScope(bean.getScope()))
+                            {
+                                contexts.add(creationalContext);
+                            }
+                            pool = ExecutorService.class.cast(beanManager.getReference(bean, ExecutorService.class, creationalContext));
+                        }
+                    }
+
+                    if (pool == null) // 2.
+                    {
+                        for (final String prefix : asList(
+                                "", "java:app/", "java:global/", "java:global/threads/", "java:global/deltaspike/", "java:"))
+                        {
+                            try {
+                                final Object instance = new InitialContext().lookup(prefix + name);
+                                if (ExecutorService.class.isInstance(instance))
+                                {
+                                    pool = ExecutorService.class.cast(instance);
+                                    break;
+                                }
+                            } catch (final NamingException e) {
+                                // no-op
+                            }
+                        }
+                    }
+
+                    if (pool == null) // 3.
+                    {
+                        final String configPrefix = "futurable.pool." + name + ".";
+                        final int coreSize = ConfigResolver.resolve(configPrefix + "coreSize")
+                                .as(Integer.class)
+                                .withDefault(Math.max(2, Runtime.getRuntime().availableProcessors()))
+                                .getValue();
+                        final int maxSize = ConfigResolver.resolve(configPrefix + "maxSize")
+                                .as(Integer.class)
+                                .withDefault(coreSize)
+                                .getValue();
+                        final long keepAlive = ConfigResolver.resolve(configPrefix + "keepAlive.value")
+                                .as(Long.class)
+                                .withDefault(0L)
+                                .getValue();
+                        final String keepAliveUnit = ConfigResolver.resolve(configPrefix + "keepAlive.unit")
+                                .as(String.class)
+                                .withDefault("MILLISECONDS")
+                                .getValue();
+
+                        final String queueType = ConfigResolver.resolve(configPrefix + "queue.type")
+                                .as(String.class)
+                                .withDefault("LINKED")
+                                .getValue();
+                        final BlockingQueue<Runnable> queue;
+                        if ("ARRAY".equalsIgnoreCase(queueType))
+                        {
+                            final int size = ConfigResolver.resolve(configPrefix + "queue.size")
+                                    .as(Integer.class)
+                                    .withDefault(1024)
+                                    .getValue();
+                            final boolean fair = ConfigResolver.resolve(configPrefix + "queue.fair")
+                                    .as(Boolean.class)
+                                    .withDefault(false)
+                                    .getValue();
+                            queue = new ArrayBlockingQueue<Runnable>(size, fair);
+                        }
+                        else if ("SYNCHRONOUS".equalsIgnoreCase(queueType))
+                        {
+                            final boolean fair = ConfigResolver.resolve(configPrefix + "queue.fair")
+                                    .as(Boolean.class)
+                                    .withDefault(false)
+                                    .getValue();
+                            queue = new SynchronousQueue<Runnable>(fair);
+                        }
+                        else/* (queueType.equalsIgnoreCase("LINKED")) */
+                        {
+                            final int capacity = ConfigResolver.resolve(configPrefix + "queue.capacity")
+                                    .as(Integer.class)
+                                    .withDefault(Integer.MAX_VALUE)
+                                    .getValue();
+                            queue = new LinkedBlockingQueue<Runnable>(capacity);
+                        }
+
+                        final String threadFactoryName = ConfigResolver.getPropertyValue(configPrefix + "threadFactory.name");
+                        final ThreadFactory threadFactory;
+                        if (threadFactoryName != null)
+                        {
+                            threadFactory = lookupByName(threadFactoryName, ThreadFactory.class);
+                        }
+                        else
+                        {
+                            threadFactory = Executors.defaultThreadFactory();
+                        }
+
+                        final String rejectedHandlerName = ConfigResolver.getPropertyValue(configPrefix + "rejectedExecutionHandler.name");
+                        final RejectedExecutionHandler rejectedHandler;
+                        if (rejectedHandlerName != null)
+                        {
+                            rejectedHandler = lookupByName(rejectedHandlerName, RejectedExecutionHandler.class);
+                        }
+                        else
+                        {
+                            rejectedHandler = new ThreadPoolExecutor.AbortPolicy();
+                        }
+
+                        pool = new ThreadPoolExecutor(
+                                coreSize, maxSize,
+                                keepAlive, TimeUnit.valueOf(keepAliveUnit),
+                                queue, threadFactory, rejectedHandler);
+                    }
+
+                    pools.put(name, pool);
+                }
+            }
         }
         return pool;
     }
 
-    private void ensureDefaultPool()
-    {
-        if (defaultPool == null)
+    private <T> T lookupByName(final String name, final Class<T> type) {
+        final Set<Bean<?>> tfb = beanManager.getBeans(name);
+        final Bean<?> bean = beanManager.resolve(tfb);
+        final CreationalContext<?> ctx = beanManager.createCreationalContext(null);
+        if (!beanManager.isNormalScope(bean.getScope()))
         {
-            synchronized (this)
-            {
-                if (defaultPool == null)
-                {
-                    defaultPool = Executors.newFixedThreadPool(
-                            Math.max(2, Runtime.getRuntime().availableProcessors()));
-                }
-            }
+            contexts.add(ctx);
         }
+        return type.cast(beanManager.getReference(bean, type, ctx));
     }
 }
 
---
+++ deltaspike/core/impl/src/test/java/org/apache/deltaspike/core/impl/future/ThreadPoolManagerTest.java
@@ -0,0 +1,116 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements. See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership. The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License. You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied. See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+package org.apache.deltaspike.core.impl.future;
+
+import org.apache.deltaspike.core.api.config.ConfigResolver;
+import org.apache.deltaspike.core.impl.config.PropertiesConfigSource;
+import org.apache.deltaspike.core.impl.scope.conversation.ConversationBeanHolder;
+import org.apache.deltaspike.core.impl.scope.viewaccess.ViewAccessBeanAccessHistory;
+import org.apache.deltaspike.core.impl.scope.viewaccess.ViewAccessBeanHolder;
+import org.apache.deltaspike.core.impl.scope.viewaccess.ViewAccessViewHistory;
+import org.apache.deltaspike.core.impl.scope.window.DefaultWindowContextQuotaHandler;
+import org.apache.deltaspike.core.impl.scope.window.WindowBeanHolder;
+import org.apache.deltaspike.core.impl.scope.window.WindowContextProducer;
+import org.apache.deltaspike.core.impl.scope.window.WindowContextQuotaHandlerCache;
+import org.apache.deltaspike.core.impl.scope.window.WindowIdHolder;
+import org.apache.deltaspike.core.spi.config.ConfigSource;
+import org.jboss.arquillian.container.test.api.Deployment;
+import org.jboss.arquillian.junit.Arquillian;
+import org.jboss.shrinkwrap.api.ShrinkWrap;
+import org.jboss.shrinkwrap.api.asset.EmptyAsset;
+import org.jboss.shrinkwrap.api.spec.JavaArchive;
+import org.jboss.shrinkwrap.api.spec.WebArchive;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import javax.inject.Inject;
+import java.util.Collections;
+import java.util.Properties;
+import java.util.concurrent.Callable;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.ThreadPoolExecutor;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertSame;
+
+@RunWith(Arquillian.class)
+public class ThreadPoolManagerTest
+{
+    @Deployment
+    public static WebArchive deploy()
+    {
+        return ShrinkWrap.create(WebArchive.class, "ThreadPoolManagerTest.war")
+                .addAsLibraries(ShrinkWrap.create(JavaArchive.class, "deltaspike-core-fake.jar")
+                    .addClasses(
+                            ThreadPoolManager.class,
+                            WindowContextProducer.class, WindowBeanHolder.class, WindowIdHolder.class,
+                            DefaultWindowContextQuotaHandler.class, WindowContextQuotaHandlerCache.class,
+                            ConversationBeanHolder.class, ViewAccessBeanHolder.class,
+                            ViewAccessBeanAccessHistory.class, ViewAccessViewHistory.class)
+                    .addAsManifestResource(EmptyAsset.INSTANCE, "beans.xml"))
+                .addAsWebInfResource(EmptyAsset.INSTANCE, "beans.xml");
+    }
+
+    @Inject
+    private ThreadPoolManager manager;
+
+    @Test
+    public void defaultPool() throws ExecutionException, InterruptedException
+    {
+        final ExecutorService auto = manager.find("auto");
+        assertEquals(auto, auto);
+        assertSame(auto, auto);
+        assertEquals(Runtime.getRuntime().availableProcessors(), ThreadPoolExecutor.class.cast(auto).getCorePoolSize());
+        assertUsable(auto);
+    }
+
+    @Test // this test validates we read the config properly but also it is lazy
+    public void configuredPool() throws ExecutionException, InterruptedException
+    {
+        ConfigResolver.addConfigSources(Collections.<ConfigSource>singletonList(new PropertiesConfigSource(new Properties()
+        {{
+            setProperty("futurable.pool.custom.coreSize", "5");
+        }})
+        {
+            @Override
+            public String getConfigName()
+            {
+                return "configuredPool";
+            }
+        }));
+        final ExecutorService custom = manager.find("custom");
+        assertEquals(custom, custom);
+        assertSame(custom, custom);
+        assertEquals(5, ThreadPoolExecutor.class.cast(custom).getCorePoolSize());
+        assertUsable(custom);
+    }
+
+    private void assertUsable(final ExecutorService pool) throws InterruptedException, ExecutionException
+    {
+        assertEquals("ok", pool.submit(new Callable<String>()
+        {
+            @Override
+            public String call() throws Exception
+            {
+                return "ok";
+            }
+        }).get());
+    }
+}
