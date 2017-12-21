11b40fec0ff29fe27e6a582a8049fa5723f98a3b
Mark Struberg <struberg@apache.org>
2017-12-21 12:49:22 +0100

    update versions for various test builds

--- deltaspike/buildall.sh
+++ deltaspike/buildall.sh
@@ -24,12 +24,23 @@
 #####################################################################################
 
 rm mvn-*log
+
+# CDI-1.0, EE6
 mvn clean install -POWB -Dowb.version=1.2.7 -Dopenejb.owb.version=1.2.7 | tee mvn-owb1_2_7.log
-mvn clean install -POWB15,OpenEJB-TomEE -Dowb.version=1.7.2 -Dopenejb.owb.version=1.7.2 -Dopenejb.version=7.0.3 | tee mvn-owb1.7.2.log
 mvn clean install -PWeld1 -Dweld.version=1.1.10.Final | tee mvn-weld1_1_10.log
-mvn clean install -PWeld1 -Dweld.version=1.1.28.Final | tee mvn-weld1_1_28.log
-mvn clean install -Ptomee-build-managed -Dtomee.version=1.7.0 -Dopenejb.version=4.7.0 -Dopenejb.owb.version=1.2.6 | tee mvn-tomee_1_7_0.log
-mvn clean install -Pjbossas-build-managed-7 | tee mvn-jbossas_7.log
+mvn clean install -Ptomee-build-managed -Dtomee.version=1.7.5 -Dopenejb.version=4.7.5 -Dopenejb.owb.version=1.2.8 | tee mvn-tomee_1_7_5.log
+
+# jbossas7 is broken on Java8, it strictly requires Java7
+# mvn clean install -Pjbossas-build-managed-7 | tee mvn-jbossas_7.log
+
+# CDI-1.2, EE7
+mvn clean install -POWB15,OpenEJB-TomEE -Dowb.version=1.7.4 -Dopenejb.owb.version=1.7.4 -Dopenejb.version=7.0.4 | tee mvn-owb1.7.4.log
+mvn clean install -PWeld2 -Dweld.version=2.4.6.Final | tee mvn-weld2_4_6.log
+mvn clean install -Pwildfly-build-managed | tee mvn-wildfly9.log
+mvn clean install -Ptomee7-build-managed,OpenEJB-TomEE -Dtomee.version=7.0.4 -Dopenejb.version=7.0.4 -Dopenejb.owb.version=1.7.4 | tee mvn-tomee_7_0_4.log
+
+# CDI-2.0, EE8
+
 
 
 # and now for the result check
