463a8c9b81f4ce3a1c64565748e25f72eeba7988
gpetracek <gpetracek@apache.org>
2018-04-16 21:27:15 +0200

    DELTASPIKE-1319 additional tests for labeled alternatives

---
+++ deltaspike/modules/test-control/impl/src/test/java/org/apache/deltaspike/test/testcontrol/uc018/LabeledTestServiceProducer.java
@@ -0,0 +1,40 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements. See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership. The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License. You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied. See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+package org.apache.deltaspike.test.testcontrol.uc018;
+
+import javax.enterprise.context.ApplicationScoped;
+import javax.enterprise.context.Dependent;
+import javax.enterprise.inject.Alternative;
+import javax.enterprise.inject.Default;
+import javax.enterprise.inject.Produces;
+
+import static org.apache.deltaspike.test.testcontrol.uc018.TestServiceQualifier.Mode.LABELED;
+
+@Alternative
+@ApplicationScoped
+public class LabeledTestServiceProducer implements InterceptedTestServiceProducer
+{
+    @Produces
+    @Default //optional
+    @Dependent
+    public InterceptedTestService createInterceptedTestService(@TestServiceQualifier(LABELED) InterceptedTestService interceptedTestService)
+    {
+        return interceptedTestService; //the exposed result is an intercepted instance
+    }
+}
